d638b87f2f13f79ea17b9842808492a888e64fe4
立石翔也 <s-tateishi@seassist.co.jp>
2025-03-25 04:31:58 +0900

    テストコミット

---
+++ src/main/java/model/DAO/UserDAO.java
@@ -0,0 +1,30 @@
+package model.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAO {
+	public UserBean getUserId(String userId)throws SQLException, ClassNotFoundException {
+		UserBean user = new UserBean();
+		String sql = """
+					SELECFT
+						user_id
+						password
+					FROM m_user
+					WHERE user_id = ?
+					""";
+		try(Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)){
+			pstmt.setString(1, userId);
+			ResultSet res = pstmt.executeQuery();
+			while(res.next()) {
+				String password = res.getString("password");
+				user.setUserId(userId);
+				user.setPassword(password);
+			}
+		}
+		return user;
+	}
+}
