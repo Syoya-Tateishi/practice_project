31fb9c0fe4d3ab8678332b33cbc4c0241bec1f97
立石翔也 <s-tateishi@seassist.co.jp>
2025-03-25 15:03:29 +0900

    地区データ取得用DAO作成

---
+++ src/main/java/model/dao/AreaDAO.java
@@ -0,0 +1,36 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entity.AreaBean;
+
+public class AreaDAO {
+	public List<AreaBean> getAllArea() throws SQLException, ClassNotFoundException {
+		List<AreaBean> areaList = new ArrayList<AreaBean>();
+		String sql = """
+					SELECT
+						area_code
+						area_name
+					FROM m_area
+					ORDER BY area_code ASC
+				""";
+		try(Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				ResultSet res = pstmt.executeQuery()){
+			while(res.next()) {
+				AreaBean area = new AreaBean();
+				area.setAreaCode(res.getString("area_code"));
+				area.setAreaName(res.getString("area_name"));
+				
+				areaList.add(area);
+			}
+		}
+
+		return areaList;
+	}
+}
