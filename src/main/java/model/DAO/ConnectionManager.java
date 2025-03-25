bc2079ab66ee4840d4f0fedae74f56695ff61010
立石翔也 <s-tateishi@seassist.co.jp>
2025-03-25 11:40:58 +0900

    修正

--- src/main/java/model/DAO/ConnectionManager.java
+++ src/main/java/model/DAO/ConnectionManager.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionManager {
+public class ConnectionManager{
 	/*
 	 * データベース接続情報
 	 * URL
--- src/main/java/model/DAO/UserDAO.java
+++ src/main/java/model/DAO/UserDAO.java
@@ -6,20 +6,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDAO {
-	public UserBean getUserId(String userId)throws SQLException, ClassNotFoundException {
+	public UserBean getUserId(String userId) throws SQLException, ClassNotFoundException {
 		UserBean user = new UserBean();
 		String sql = """
-					SELECFT
-						user_id
-						password
-					FROM m_user
-					WHERE user_id = ?
-					""";
-		try(Connection con = ConnectionManager.getConnection();
-				PreparedStatement pstmt = con.prepareStatement(sql)){
+				SELECFT
+					user_id
+					password
+				FROM m_user
+				WHERE user_id = ?
+				""";
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)) {
 			pstmt.setString(1, userId);
 			ResultSet res = pstmt.executeQuery();
-			while(res.next()) {
+			while (res.next()) {
 				String password = res.getString("password");
 				user.setUserId(userId);
 				user.setPassword(password);
