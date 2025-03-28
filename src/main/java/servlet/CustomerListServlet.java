8421fbf588856cc29ebe116e83610bd0534f8cb8
立石翔也 <s-tateishi@seassist.co.jp>
2025-03-28 09:32:04 +0900

    ListServlet

--- src/main/java/servlet/CustomerListServlet.java
+++ src/main/java/servlet/CustomerListServlet.java
@@ -1,11 +1,21 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.dao.AreaDAO;
+import model.dao.CustomerDAO;
+import model.entity.AreaBean;
+import model.entity.CustomerBean;
 
 /**
  * Servlet implementation class CustomerListServlet
@@ -25,9 +35,37 @@ public class CustomerListServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+request.setCharacterEncoding("UTF-8");
+		
+		HttpSession session = request.getSession();
+		String userId = (String) session.getAttribute("userId");
+		String password = (String) session.getAttribute("password");
+		
+		if(userId == "null" || password == null) {
+			String message = "セッション切れ";
+			request.setAttribute("message", message);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+			dispatcher.forward(request, response);
+		}
+		
+		CustomerDAO customer = new CustomerDAO();
+		AreaDAO area = new AreaDAO();
+		List<CustomerBean> customerList = new ArrayList<CustomerBean>();
+		List<AreaBean> areaList = new ArrayList<AreaBean>();
+		
+		try {
+			customerList = customer.getAllCustomer();
+			areaList = area.getAllArea();
+			request.setAttribute("customerList", customerList);
+			request.setAttribute("areaList", areaList);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("employeeList.jsp");
+			dispatcher.forward(request, response);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
